67a0c40a8aefc0cd599df7f3b558306f3b625b88
achmaddav <achmaddavit92@gmail.com>
2022-12-16 07:09:35 +0700

    Initial commit

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,31 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Next Comment");
+        // memanggil method
+        myFavoriteFood();
+        // memanggil method
+        myFavoritePlace();
+    }
+
+    // ini method
+    public static void myFavoriteFood(){
+        System.out.println("My Favorite food:");
+        System.out.println("1. Bakso");
+        System.out.println("2. Mei Goreng Tante (Tanpa Telor)");
+        System.out.println("3. Mei Ayam");
+    }
+
+    // favorite place
+    public static void myFavoritePlace(){
+        System.out.println("My Favorite food:");
+        System.out.println("1. Bandung");
+        System.out.println("2. Medang Belawan");
+        System.out.println("3. Yogyakarta - Malioboro");
+        System.out.println("4. Solo - Balapan");
+    }
+    // favorite Friends
+    // favorite material
+    // favorite Hobby
+}
+
